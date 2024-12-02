278bccb060d02d7d77b2a5cf9be0574c61440a77
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-02 11:19:31 +0100

    template engine added

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send("hello world");
+  res.render("index");
 });
 
 
